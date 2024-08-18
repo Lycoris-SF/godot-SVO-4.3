bf78bfcee6c09dd4ada792754a9f332b5050a8cd
CSEE Lyco <hh23395@essex.ac.uk>
2024-08-18 17:03:56 +0100

    Import VS settings and switch to godot-4.3

--- src/register/register_types.h
+++ src/register/register_types.h
@@ -1,3 +1,11 @@
-#pragma once
-void initialize_svo_module();
-void uninitialize_svo_module();
+#ifndef GDEXAMPLE_REGISTER_TYPES_H
+#define GDEXAMPLE_REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_svo_module(ModuleInitializationLevel p_level);
+void uninitialize_svo_module(ModuleInitializationLevel p_level);
+
+#endif // GDEXAMPLE_REGISTER_TYPES_H
